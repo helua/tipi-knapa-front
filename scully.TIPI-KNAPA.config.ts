778399909177ec07ba378da8c9d9ec80ebe93014
helua <helua@users.noreply.github.com>
2021-11-25 17:25:56 +0100

    scully config deleted

--- scully.TIPI-KNAPA.config.ts
+++
@@ -1,8 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "tipi-knapa",
-  outDir: './dist/static',
-  routes: {
-  }
-};
